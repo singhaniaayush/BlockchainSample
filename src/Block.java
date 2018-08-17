14a286669a57995c91ee6d707488fcc5a5afae32
Ayush Singhania <ayush.singhania@srkaycg.com>
2018-08-17 11:01:14 +0530

    Commented Block Class

--- src/Block.java
+++ src/Block.java
@@ -1,12 +1,34 @@
+/*
+ * 
+ * @author Ayush Singhania
+ * 
+ */
 import java.util.Date;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Block.
+ */
 public class Block {
 
+	/** The Hash. */
 	public String Hash;
+	
+	/** The prev hash. */
 	public String prevHash;
+	
+	/** The data. */
 	private String data;
+	
+	/** The time stamp. */
 	private long timeStamp;
 	
+	/**
+	 * Instantiates a new block.
+	 *
+	 * @param data the data
+	 * @param prevHash the prev hash
+	 */
 	public Block(String data, String prevHash) {
 		this.data = data;
 		this.prevHash = prevHash;
