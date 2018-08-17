de34e84ded957ca74ed354fd793fd8162232cf74
Ayush Singhania <ayush.singhania@srkaycg.com>
2018-08-17 11:54:21 +0530

    first commit

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -7,9 +7,9 @@ public class BlockChain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Block genesis = new Block("First Block","0");
+		Block genesis = new Block("Genesis Block","0");
 		Block first = new Block("First Block",genesis.Hash);
-		Block second = new Block("First Block",first.Hash);
+		Block second = new Block("Second Block",first.Hash);
 		
 		System.out.println("Genesis hash: " + genesis.Hash);
 		System.out.println("Genesis prev hash: " + genesis.prevHash);
