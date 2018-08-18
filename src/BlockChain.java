ade2dd3acc653821d89ea27f60a2dc1beafc39d8
Ayush Singhania <ayush.singhania@srkaycg.com>
2018-08-18 07:15:47 +0530

    gson and arraylist

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,13 +1,21 @@
-
 /**
  * The Class BlockChain.
  */
+
+import java.util.ArrayList;
+
+import com.google.gson.GsonBuilder;
+
 public class BlockChain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Block genesis = new Block("Genesis Block","0");
+		//Normal blocks
+		
+		/*Block genesis = new Block("Genesis Block","0");
 		Block first = new Block("First Block",genesis.Hash);
 		Block second = new Block("Second Block",first.Hash);
 		
@@ -19,7 +27,16 @@ public class BlockChain {
 		System.out.println();
 		System.out.println("Second hash: " +second.Hash);
 		System.out.println("Second hash: " +second.prevHash);
+		*/
 		
+		//ArrayList blocks
+		blockchain.add(new Block("Genesis Block","0"));
+		blockchain.add(new Block("First Block", blockchain.get(blockchain.size()-1).Hash));
+		blockchain.add(new Block("Second Block", blockchain.get(blockchain.size()-1).Hash));
+		
+		//Displaying in JSON format
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		System.out.println(blockchainJson);
 		
 
 	}
